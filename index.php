efe17575c57172234e37697aa234cd66ebc8f3e9
RasmusDesign <bastien.mottier@gmail.com>
2015-06-08 16:38:38 +0200

    affichage de la date a côté des articles.

--- index.php
+++ index.php
@@ -8,20 +8,25 @@ $count = 1;
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <div class="col-md-6">
                 <div class="article_container">
-                <a href="<?php the_permalink(); ?>"><div class="article_image" style="background-image:url(<?php echo catch_that_image() ?>);"></div></a>
-                <div class="article_desc">
-                    <h1 class="hyphenate"><a href="<?php the_permalink(); ?>"><img src="<?php echo get_template_directory_uri() ;?>/bootstrap/img/cellule.png" class="cellule" alt="<?php  echo get_the_title(); ?>"/><?php echo shortened_title(); ?></a></h1>
-                    <div class="separator"></div>
-                    <?php the_excerpt(); ?>
+                    <a href="<?php the_permalink(); ?>">
+                        <div class="article_image" style="background-image:url(<?php echo catch_that_image() ?>);">
+                            <p class="article_date"><?php echo get_the_modified_date(); ?></p>
+                        </div>
+                    </a>
+                    <div class="article_desc">
+                        <h1 class="hyphenate"><a href="<?php the_permalink(); ?>"><img src="<?php echo get_template_directory_uri() ;?>/bootstrap/img/cellule.png" class="cellule" alt="<?php  echo get_the_title(); ?>"/><?php echo shortened_title(); ?></a></h1>
+                        <div class="separator"></div>
+                        <?php the_excerpt(); ?>
+                    </div>
                 </div>
             </div>
-            </div>
-            <?php if($count%2==0){ ?>
-                </div><div class="row">
-            <?php }
-                $count ++;
-                endwhile; else: 
-            ?>
+
+        <?php if($count%2==0){ ?>
+        </div><div class="row">
+        <?php }
+$count ++;
+endwhile; else:
+        ?>
         <p><?php _e('Sorry, this page does not exist.'); ?></p>
         <?php endif; ?>
         </div>
