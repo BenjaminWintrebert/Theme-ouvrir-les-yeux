82bc700df8658f891a803da20bc629ba7e90e56f
RasmusDesign <bastien.mottier@gmail.com>
2015-06-08 13:14:59 +0200

    photo vers la droite

--- sidebar.php
+++ sidebar.php
@@ -2,11 +2,11 @@
     <div class="col-md-12">
         <h1>Olivier Gourmet nous parraine</h1>
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-5">
                 <img src="<?php echo get_template_directory_uri() ;?>/bootstrap/img/Sans%20titre-1.png" class="img-responsive round border-red" />
             </div>
-            <div class="col-md-8">
-                <p>Comme moi, Soutenez Ouvrir Les Yeux</p>
+            <div class="col-md-7">
+                <p class="devise">Comme moi, <br>Soutenez <br>Ouvrir Les Yeux</p>
             </div>
         </div>
     </div>
