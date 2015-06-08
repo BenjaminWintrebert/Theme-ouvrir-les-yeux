cfe7bcdd744ee8a542f98e99d5d9b1f6af601207
RasmusDesign <bastien.mottier@gmail.com>
2015-06-08 12:43:51 +0200

    Police comfortaa, sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,13 @@
-<div class="row">
+<div class="row sidebar">
     <div class="col-md-12">
-        Olivier Gourmet nous parraine
+        <h1>Olivier Gourmet nous parraine</h1>
+        <div class="row">
+            <div class="col-md-4">
+                <img src="<?php echo get_template_directory_uri() ;?>/bootstrap/img/Sans%20titre-1.png" class="img-responsive round border-red" />
+            </div>
+            <div class="col-md-8">
+                <p>Comme moi, Soutenez Ouvrir Les Yeux</p>
+            </div>
+        </div>
     </div>
 </div>
