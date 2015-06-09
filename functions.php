b34244f3b2494dbea6e1aa1cbf638113ec3ecd68
RasmusDesign <bastien.mottier@gmail.com>
2015-06-09 11:11:34 +0200

    many changes

--- functions.php
+++ functions.php
@@ -162,4 +162,12 @@ function my_search() {
 add_action( 'wp_ajax_my_search', 'my_search' );
 add_action( 'wp_ajax_nopriv_my_search', 'my_search' );
 
+if ( function_exists('register_sidebar') )
+    register_sidebar(array('name'=>'Sidebar',
+                           'before_widget' => '<div>',
+                           'after_widget' => '</div>',
+                           'before_title' => '<h3>',
+                           'after_title' => '</h3>',
+                          ));
+
 ?>
